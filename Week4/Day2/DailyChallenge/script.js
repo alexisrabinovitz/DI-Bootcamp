dcde93493d0cb80c846b2920eefe80f0819942f4
alexisrabinovitz <alerabinovitz@gmail.com>
2023-02-27 19:47:05 -0600

    Day 2

--- Week4/Day2/DailyChallenge/script.js
+++ Week4/Day2/DailyChallenge/script.js
@@ -3,18 +3,20 @@
 // Write a JavaScript program that recreates the pattern below.
 // Do this Daily Challenge by youself, without looking at the answers on the internet.
 
-const maxNumber = 6
-let stars = ""
-for (let i = 0; i < maxNumber; i++){
-    stars= stars + "*"
-    console.log (stars)
-}
+// const maxNumber = 6
+// let stars = ""
+// for (let i = 0; i < maxNumber; i++){
+//     stars= stars + "*"
+//     console.log (stars)
+// }
 
-let starse = ""
-for (let i = 0; i <  6; i++){
-    starse= starse + "*"
-    console.log (starse)
-}// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+// let starse = ""
+// for (let i = 0; i <  6; i++){
+//     starse= starse + "*"
+//     console.log (starse)
+// }
+
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
 // no recomendable lo siguiente: 
 const maxNumber = 6
 
